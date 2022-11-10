98478084ff65bf2a7be295baeae474ad26474bff
leonel-ruiz-villacis <lruiz19@jaumebalmes.net>
2022-11-10 20:35:29 +0100

    practica6

---
+++ ej4/conc.py
@@ -0,0 +1,20 @@
+'''
+#Imprimir el resultat de la suma de les dues variables (int)
+a = 10
+b = 20
+suma = a + b
+print(suma)'''
+
+
+'''Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)'''
+n1=int(input("Intro número uno: "))
+n2=int(input("Intro numero dos: "))
+suma=n1+n2
+
+print("La suma de los dos números es: " + str(n1+n2))
+
+
+
+
+
+
---
+++ ej6/ej6.py
@@ -0,0 +1,7 @@
+def suma(a, b):
+    return int(a) + int(b)
+
+x = input("Type the first number: ")
+y = input("Type the second number: ")
+
+print("La suma del valor " + x + " mès el valor " + y + " serà: " + x + "+" + y + "=" + str(suma(x, y)) + ".")
